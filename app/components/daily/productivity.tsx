653ff2c69929f2ad80242d92ce7e1479d33427de
Roy Anger <roy@royanger.com>
2021-12-06 23:39:22 -0500

    Basic form submission for productibity working

--- app/components/daily/productivity.tsx
+++ app/components/daily/productivity.tsx
@@ -1,6 +1,8 @@
 import { HeaderTwo } from '../headlines'
 import Radio from '../forms/radio'
 import { findNotesEntries } from '~/queries/findNotes'
+import { Form } from 'remix'
+import Button from '../button'
 
 interface Productivity {
   entries: {
@@ -22,6 +24,7 @@ export default function Productivity({ entries }: Productivity) {
         <div key={index}>
           <Radio
             value={index}
+            name={index + 1}
             checked={index + 1 <= entries.score ? true : false}
           />
         </div>
@@ -31,7 +34,7 @@ export default function Productivity({ entries }: Productivity) {
     radioInputs = items.map((item, index) => {
       return (
         <div key={index}>
-          <Radio value={index} checked={false} />
+          <Radio value={index} name="newproductivity" checked={false} />
         </div>
       )
     })
@@ -41,22 +44,27 @@ export default function Productivity({ entries }: Productivity) {
     <>
       <HeaderTwo>Productivity Score?</HeaderTwo>
       <p>Rate how you productive you felt out of 10.</p>
-      {/* <p>From DB: {data?.productivity && data.productivity[0]?.score}</p> */}
-      <div className="flex-shrink flex">
-        <div className="grid grid-cols-10 mb-6">
-          {radioInputs}
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-          <div>7</div>
-          <div>8</div>
-          <div>9</div>
-          <div>10</div>
+      <Form method="post" action="/daily/planner">
+        <input type="hidden" name="formType" value="productivity" />
+        <div className="flex-shrink flex">
+          <div className="grid grid-cols-12 mb-6">
+            {radioInputs}
+            <div className="row-start-1 row-end-3 col-start-11 col-end-13">
+              <Button type="submit" title="Save" />
+            </div>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+            <div>7</div>
+            <div>8</div>
+            <div>9</div>
+            <div>10</div>
+          </div>
         </div>
-      </div>
+      </Form>
     </>
   )
 }
