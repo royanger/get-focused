0913db01f3317aece48c91f0ae06e67f7af5eb6a
Roy Anger <roy@royanger.com>
2022-01-02 19:47:23 -0500

    REfactored some parts of components, added schema for all Models, Wins working

--- app/routes/weekly/review.tsx
+++ app/routes/weekly/review.tsx
@@ -5,28 +5,51 @@ import {
   useActionData,
 } from 'remix'
 import { authenticator } from '~/services/auth.server'
-import { findWeeklyReview } from '~/queries/findWeeklyReview'
+import findWeeklyWin from '~/queries/findWeeklyWin'
 
 // components
 import Container from '~/components/container'
 import { HeaderOne, HeaderTwo } from '~/components/headlines'
 import ReviewElement from '~/components/weekly/reviewElement'
-import Improvements from '~/components/weekly/improvements'
+import ListSection from '~/components/weekly/ListSection'
+import { findOrCreateDate } from '~/queries/findOrCreateDate'
 
 export let loader: LoaderFunction = async ({ request }) => {
   let user = await authenticator.isAuthenticated(request)
 
-  console.log('working?')
+  const dateResults = await findOrCreateDate('today')
+  const win = findWeeklyWin(dateResults.id, user.id)
 
-  const results = await findWeeklyReview('today', user.id)
+  const data = {}
+  await Promise.all([win]).then(results => {
+    data.win = results[0]
+  })
+  return data
+}
 
-  console.log('results', results)
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData()
 
-  return 'test'
+  if (formData.get('type') === 'wins') {
+    console.log('FORMTYPE', 'wins')
+  }
+  if (formData.get('type') === 'improvement') {
+    console.log('FORMTYPE', 'improvement')
+  }
+  if (formData.get('type') === 'learningpoints') {
+    console.log('FORMTYPE', 'learningpoints')
+  }
+  if (formData.get('type') === 'refocus') {
+    console.log('FORMTYPE', 'refocus')
+  }
+  console.log('form data', formData)
+
+  return null
 }
 
 export default function WeeklyReview() {
   const data = useLoaderData()
+  console.log('DATA IN COMPONENT', data.win.id)
 
   const items = [
     {
@@ -47,20 +70,26 @@ export default function WeeklyReview() {
 
           <p>What was great about your week? What was a solid win for you?</p>
 
-          <ReviewElement id="asdfsdf" win="Enter a win from the week" />
+          <ReviewElement
+            id={data?.win?.id}
+            win={data?.win?.item}
+            formType="wins"
+          />
 
-          <Improvements
-            items={items}
+          <ListSection
+            items={data?.improvements}
             errors={null}
             title="Tasks and Areas to Improve"
             info="What tasks were not completed? What areas can you improve next week?"
+            formType="improvements"
           />
 
-          <Improvements
+          <ListSection
             items={items}
             errors={null}
             title="Learning Points"
             info="List the things that you learned from or the ways you improved this week."
+            formType="learningpoints"
           />
 
           <HeaderTwo>Refocus for Next Week</HeaderTwo>
@@ -69,6 +98,7 @@ export default function WeeklyReview() {
           <ReviewElement
             id="asdfsdf"
             win="What is the most important thing to refocus on for next week?"
+            formType="refocus"
           />
         </div>
       </Container>
