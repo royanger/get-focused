d87e017b944092b3a31ad039dd5b100610b6f916
Roy Anger <roy@royanger.com>
2022-01-28 03:44:34 -0500

    Initial work on correcting Note behaviour.
    This more or less works, except it seems to cause or trigger the transition.state === loading a bunch, which causes too many re-renders errors.

--- app/components/forms/noteEl.tsx
+++ app/components/forms/noteEl.tsx
@@ -1,19 +1,41 @@
 import NoteSave from './noteSave'
 import NoteCancel from './noteCancel'
-import { Form } from 'remix'
+import { Form, useTransition } from 'remix'
 import { Note } from '~/interfaces'
+import React from 'react'
 
-export default function NoteEl({ id, dateId, note }: Note) {
+export default function NoteEl({ id, note }: Note) {
+  const transition = useTransition()
+  console.log(transition.state)
+
+  const [formValue, setFormValue] = React.useState(note)
+
+  const text =
+    transition.state === 'submitting'
+      ? 'Saving...'
+      : transition.state === 'loading'
+      ? 'Saved!'
+      : 'Go'
+
+  function handleChange(e) {
+    setFormValue(e.target.value)
+  }
+
+  if (transition.state === 'loading' && id === 'note-new') {
+    console.log('RESET FORM')
+    //  setFormValue('sjkadflasjfkldj')
+  }
   return (
     <>
       <Form method="post" action="/daily/planner">
         <input type="hidden" name="formType" value="note" />
         <input type="hidden" name="id" value={id} />
         <textarea
-          defaultValue={note}
+          value={formValue}
           name="message"
           className="w-full h-36 border-2 border-purple rounded p-2"
           placeholder="Enter your thoughts and notes here..."
+          onChange={e => handleChange(e)}
         />
         <NoteSave />
 
