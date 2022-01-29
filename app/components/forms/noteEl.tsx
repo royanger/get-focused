a97b0302e4481179aefa2127b9a913945905c362
Roy Anger <roy@royanger.com>
2022-01-28 21:43:36 -0500

    Converted from managed state to using useRef and problem is resolved.

--- app/components/forms/noteEl.tsx
+++ app/components/forms/noteEl.tsx
@@ -1,41 +1,32 @@
+import React from 'react'
 import NoteSave from './noteSave'
 import NoteCancel from './noteCancel'
 import { Form, useTransition } from 'remix'
 import { Note } from '~/interfaces'
-import React from 'react'
 
 export default function NoteEl({ id, note }: Note) {
   const transition = useTransition()
-  console.log(transition.state)
+  let formRef = React.useRef()
+  let isAdding =
+    transition.state === 'submitting' &&
+    transition.submission.formData.get('formType') === 'note'
 
-  const [formValue, setFormValue] = React.useState(note)
+  React.useEffect(() => {
+    if (!isAdding) {
+      formRef.current.reset()
+    }
+  }, [isAdding])
 
-  const text =
-    transition.state === 'submitting'
-      ? 'Saving...'
-      : transition.state === 'loading'
-      ? 'Saved!'
-      : 'Go'
-
-  function handleChange(e) {
-    setFormValue(e.target.value)
-  }
-
-  if (transition.state === 'loading' && id === 'note-new') {
-    console.log('RESET FORM')
-    //  setFormValue('sjkadflasjfkldj')
-  }
   return (
     <>
-      <Form method="post" action="/daily/planner">
+      <Form ref={formRef} method="post" action="/daily/planner" id={id}>
         <input type="hidden" name="formType" value="note" />
         <input type="hidden" name="id" value={id} />
         <textarea
-          value={formValue}
+          defaultValue={note}
           name="message"
           className="w-full h-36 border-2 border-purple rounded p-2"
           placeholder="Enter your thoughts and notes here..."
-          onChange={e => handleChange(e)}
         />
         <NoteSave />
 
