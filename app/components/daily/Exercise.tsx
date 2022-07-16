b3ca80ca786af6302384df073e5e90a28b9fa057
Roy Anger <roy@royanger.com>
2022-07-16 03:08:44 -0400

    Refactored exercies so they would display reliably

--- app/components/daily/Exercise.tsx
+++ app/components/daily/Exercise.tsx
@@ -1,10 +1,20 @@
+import * as React from 'react'
 import { Form, useSearchParams, useTransition } from '@remix-run/react'
 import Button from '../Button'
-import Radio from '../forms/Radio'
 import { HeaderTwo } from '../Headlines'
 
 export default function Exercise({ exercise, errors }: Exercise) {
+  const [status, setStatus] = React.useState<boolean | undefined>()
+  //  exercise?.completed ? exercise.completed : undefined
   const transition = useTransition()
+
+  React.useEffect(() => {
+    if (exercise !== undefined) {
+      setStatus(exercise?.completed)
+    } else if (exercise === undefined) {
+      setStatus(undefined)
+    }
+  }, [exercise])
 
   const [searchParams] = useSearchParams()
   const paramDate = searchParams.get('date')
@@ -17,6 +27,15 @@ export default function Exercise({ exercise, errors }: Exercise) {
         transition?.submission?.formData.get('formType') === 'exercise'
       ? { text: 'Saved!', variant: 'saved' }
       : { text: 'Save', variant: 'default' }
+
+  const handleChange = (newStatus: boolean) => {
+    setStatus(newStatus)
+  }
+
+  const yesStatus =
+    status !== undefined ? (status === true ? true : false) : false
+  const noStatus =
+    status !== undefined ? (status === false ? true : false) : false
 
   return (
     <div className="m-2 p-3 border-0 rounded-lg shadow-md shadow-purple-100">
@@ -33,34 +52,46 @@ export default function Exercise({ exercise, errors }: Exercise) {
             value={exercise?.id ? exercise.id : 'new'}
           />
           <div>
-            <div key="yes">
-              <Radio
+            <div>
+              <input
+                id="exercise-yes"
                 value="yes"
+                type="radio"
                 name="exercise"
-                checked={exercise?.completed === true ? true : false}
+                checked={yesStatus}
+                className="outline-none focus:ring-0 border-[1px] border-purple text-purple"
+                onChange={() => handleChange(true)}
               />
+              <label htmlFor="exercise-yes">Yes</label>
             </div>
-            <div key="no">
-              <Radio
+            <div>
+              <input
+                id="exercise-no"
                 value="no"
+                type="radio"
                 name="exercise"
-                checked={exercise?.completed === false ? true : false}
+                checked={noStatus}
+                className="outline-none focus:ring-0 border-[1px] border-purple text-purple"
+                onChange={() => handleChange(false)}
               />
+              <label htmlFor="exercise-no">No</label>
             </div>
           </div>
 
-          <div className="mt-8">
-            <Button
-              type="submit"
-              title={buttonState.text}
-              variant={buttonState.variant}
-              size="sm"
-            />
+          <div className="mt-8 flex flex-row items-center justify-center">
+            <div>
+              <Button
+                type="submit"
+                title={buttonState.text}
+                variant={buttonState.variant}
+                size="sm"
+              />
+            </div>
+            <div className="text-sm text-error ml-4 h-5 grow">
+              {errors ? errors.message : ''}
+            </div>
           </div>
         </div>
-        {errors
-          ? `<div className="text-sm text-error mb-6 h-5">${errors.message}</div>`
-          : ''}
       </Form>
     </div>
   )
